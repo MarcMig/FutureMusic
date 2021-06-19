0fa602a3326199831fa1d3b9fb5b56411ea4fd1a
fedesigns <ft616@ic.ac.uk>
2021-06-19 13:28:01 +0100

    added timeout error handling, extended wait limit

--- get_urls_slowly.py
+++ get_urls_slowly.py
@@ -6,25 +6,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 
-def scrape_song_urls(n = 'all', verbose=True):
+
+def scrape_song_urls(n="all", verbose=True):
 
     # Get list song Urls from DataFrame
-    song_urls = pd.read_csv('tracks_and_beats.csv', usecols=["track_name","track_url"])
+    song_urls = pd.read_csv("tracks_and_beats.csv", usecols=["track_name", "track_url"])
     song_urls_df = pd.DataFrame()
     options = webdriver.ChromeOptions()
     options.add_argument("--ignore-certificate-error")
     options.add_argument("--ignore-ssl-errors")
     driver = webdriver.Chrome(options=options)
 
-    if n != 'all': song_urls = song_urls.iloc[:n,]
+    if n != "all":
+        song_urls = song_urls.iloc[
+            :n,
+        ]
 
-    url = 'https://loader.to/en3/soundcloud-downloader.html'
+    url = "https://loader.to/en3/soundcloud-downloader.html"
 
     for i in range(len(song_urls)):
 
         # Go to webpage, find elements
-        track_name = song_urls['track_name'][i]
-        track_url = song_urls['track_url'][i]
+        track_name = song_urls["track_name"][i]
+        track_url = song_urls["track_url"][i]
         print(track_name, track_url)
 
         driver.get(url)
@@ -34,35 +38,47 @@ def scrape_song_urls(n = 'all', verbose=True):
         sleep(1)
         driver.find_element_by_xpath('//*[@id="load"]').click()
 
-        wait = WebDriverWait(driver, 50)
-        wait.until(presence_of_element_located((By.XPATH,'//div[contains(@class, "download-card")]')))
-        element_with_string = driver.find_element_by_xpath('//div[contains(@class, "download-card")]')
+        try:
+            wait = WebDriverWait(driver, 100)
+            wait.until(
+                presence_of_element_located(
+                    (By.XPATH, '//div[contains(@class, "download-card")]')
+                )
+            )
+            element_with_string = driver.find_element_by_xpath(
+                '//div[contains(@class, "download-card")]'
+            )
 
-        # Get element Id string from Download box id
-        string = element_with_string.get_attribute('id').split('-')[1]
-        downloadbutton_id = f"{string}_downloadLink"
-        downloadbutton_xpath = f"//*[@id='{downloadbutton_id}']"
+            # Get element Id string from Download box id
+            string = element_with_string.get_attribute("id").split("-")[1]
+            downloadbutton_id = f"{string}_downloadLink"
+            downloadbutton_xpath = f"//*[@id='{downloadbutton_id}']"
 
-        # Get Download URL using xpath generated previously
-        dl_button = f"{string}_downloadButton"
-        dl_button_xpath = f"//*[@id='{dl_button}']"
+            # Get Download URL using xpath generated previously
+            dl_button = f"{string}_downloadButton"
+            dl_button_xpath = f"//*[@id='{dl_button}']"
 
-        wait.until(element_to_be_clickable((By.XPATH, dl_button_xpath)))
-        dl_url = driver.find_element_by_xpath(downloadbutton_xpath).get_attribute('href')
+            wait.until(element_to_be_clickable((By.XPATH, dl_button_xpath)))
+            dl_url = driver.find_element_by_xpath(downloadbutton_xpath).get_attribute(
+                "href"
+            )
 
-        extra_data = {"Track_name": track_name,        
-                        "download_url": dl_url}
-        
-        print(extra_data)
+            extra_data = {"Track_name": track_name, "download_url": dl_url}
 
-        # Append data to lists and Save
+            print(extra_data)
 
-        song_urls_df = song_urls_df.append(extra_data, ignore_index=True) 
-        song_urls_df.to_csv(f'track_urls_{n}.csv')
+            # Append data to lists and Save
 
-        if verbose: print('Got url for: ' + track_name)
+            song_urls_df = song_urls_df.append(extra_data, ignore_index=True)
+            song_urls_df.to_csv(f"track_urls_{n}.csv")
+
+            if verbose:
+                print("Got url for: " + track_name)
+        except TimeoutError:
+            print("Wait period lapsed")
+            continue
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     scrape_song_urls(n=10)
