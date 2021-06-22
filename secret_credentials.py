4e1ce13b946d43ddf1b68d1de14c5bf74ba0251f
Marc Mignot <marc.migot@gmail.com>
2021-06-22 15:57:06 +0100

    removed creds

--- secret_credentials.py
+++
@@ -1,8 +0,0 @@
-username = "fedesign"
-access_key_ID = "AKIA3GK2UDC443WSNLVG"
-secret_access_ID = "5J4xBtKgALwNGlDcNq4ztGfgIbsDOmS2/Z1HDd4t"
-
-db_endpoint = "sound-scraping.c2egw1b84ra5.eu-west-3.rds.amazonaws.com"
-db_port = int(5432)
-password = ""  # test whether a password is needed, it shouldn't be
-database = "postgres"
