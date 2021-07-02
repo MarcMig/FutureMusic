17679ffcaf5e728a713bbc7edde93dd8e4fd0a3c
fedesigns <ft616@ic.ac.uk>
2021-07-02 14:57:31 +0200

    added data for ec2 testing

---
+++ Lambda/Lambda/index.js
@@ -0,0 +1,68 @@
+// 1. for logs, add cloudwatch permissions to lambda
+// 2. for copying, add s3 bucket permissions to lambda: s3:putObject
+// 3. p
+
+
+const { PassThrough } = require('stream');
+const AWS = require('aws-sdk');
+const superagent = require('superagent');
+
+const BUCKET_NAME = 'sound-scraping';
+
+AWS.config.update({ region: process.env.AWS_REGION });
+
+const s3 = new AWS.S3();
+
+const putStreamToS3 = async (s3Service, { Bucket, Key, ContentType, Metadata, Body }) => {
+  console.log(`[AWSUtils.putStreamToS3] Putting ${Key} as stream into ${Bucket}`);
+  return s3Service
+    .upload({
+      Bucket,
+      Key,
+      ContentType,
+      Metadata,
+      Body,
+    })
+    .promise();
+};
+
+exports.handler = async (event) => {
+  const { originUrls } = event;
+
+  const result = await Promise.all(originUrls.map((el) => {
+    const { id, name, url } = el;
+    let upload = null;
+    try {
+      // these are the parameters
+      const stream = new PassThrough();
+      const req = superagent.post(url).set({ 'User-Agent': 'Mozilla/5.0' })
+                                      .send("");
+      const fileName = `track-files/${id}.wav`;
+
+      const wavMimeType = 'audio/wav';
+
+      // create an upload to s3
+      upload = putStreamToS3(s3, {
+        Bucket: BUCKET_NAME,
+        Key: fileName,
+        Metadata: {'name': name},
+        ContentType: wavMimeType,
+        Body: stream,
+      });
+
+      // pipe the request (origin) to the stream (which is the s3 upload's body)
+      req.pipe(stream);
+      console.log('Piped origin to destination')
+    } catch (error) {
+      console.error(`Error piping request for ${url}`)
+    }
+
+    // if you want to wait for each request, comment out this line below, and uncomment the line after
+    return upload;
+    // await upload;
+    // return true;
+  }));
+
+  console.log('Copy results');
+  console.log(result);
+}
--- get_urls_futures.py
+++ get_urls_futures.py
@@ -10,13 +10,14 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 from concurrent.futures import ThreadPoolExecutor, wait
 
-class FutureScraper():
 
-    def __init__(self, csv_name, start=0, end='all',  headless=True, threads=1):
+class FutureScraper:
+    def __init__(self, csv_name, start=0, end="all", headless=True, threads=1):
         self.start = start
         self.end = end
-        self.song_urls_df = pd.read_csv(csv_name,
-        usecols=["track_name", "track_id", "track_url", "artist_name", "artist_id"]
+        self.song_urls_df = pd.read_csv(
+            csv_name,
+            usecols=["track_name", "track_id", "track_url", "artist_name", "artist_id"],
         )
         self.dl_urls_df = pd.DataFrame()
         self.url = "https://loader.to/en3/soundcloud-downloader.html"
@@ -29,32 +30,47 @@ class FutureScraper():
         while connection_attemps < 3:
             try:
                 browser.get(url)
-                WebDriverWait(browser, 5).until(presence_of_element_located((By.XPATH, '//*[@id="format"]/optgroup[1]/option[8]')))
-                
+                WebDriverWait(browser, 5).until(
+                    presence_of_element_located(
+                        (By.XPATH, '//*[@id="format"]/optgroup[1]/option[8]')
+                    )
+                )
+
                 return True
             except Exception as e:
                 print(e)
                 connection_attemps += 10
-                print(f'Error connecting to {url}')
-                print(f'Attempt # {connection_attemps}')
+                print(f"Error connecting to {url}")
+                print(f"Attempt # {connection_attemps}")
         return False
 
     def write_to_file(self, dl_url, i, filename):
 
-            track_name = self.song_urls_df["track_name"][i]
-            track_id = self.song_urls_df["track_id"][i]
-            artist_name = self.song_urls_df["artist_name"][i]
-            artist_id = self.song_urls_df["artist_id"][i]
+        track_name = self.song_urls_df["track_name"][i]
+        track_id = self.song_urls_df["track_id"][i]
+        artist_name = self.song_urls_df["artist_name"][i]
+        artist_id = self.song_urls_df["artist_id"][i]
 
-            data = {"id": i, "track_name": track_name, "track_id": track_id,
-                "download_url": dl_url, "artist_name": artist_name, "artist_id": artist_id
-                }
-                        
-            with open(filename, "a") as csvfile:
-                fieldnames = ["id", "track_name", "track_id", "download_url", "artist_name", "artist_id"]
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writerow(data)
+        data = {
+            "id": i,
+            "track_name": track_name,
+            "track_id": track_id,
+            "download_url": dl_url,
+            "artist_name": artist_name,
+            "artist_id": artist_id,
+        }
 
+        with open(filename, "a") as csvfile:
+            fieldnames = [
+                "id",
+                "track_name",
+                "track_id",
+                "download_url",
+                "artist_name",
+                "artist_id",
+            ]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow(data)
 
     def run_process(self, i, output_filename):
 
@@ -62,7 +78,9 @@ class FutureScraper():
 
         if self.connect_to_base(browser, self.url):
             sleep(1)
-            browser.find_element_by_xpath('//*[@id="link"]').send_keys(self.song_urls_df["track_url"][i])
+            browser.find_element_by_xpath('//*[@id="link"]').send_keys(
+                self.song_urls_df["track_url"][i]
+            )
             sleep(1)
             browser.find_element_by_xpath('//*[@id="load"]').click()
 
@@ -87,30 +105,32 @@ class FutureScraper():
                 dl_button_xpath = f"//*[@id='{dl_button}']"
 
                 wait.until(element_to_be_clickable((By.XPATH, dl_button_xpath)))
-                dl_url = browser.find_element_by_xpath(downloadbutton_xpath).get_attribute(
-                    "href"
-                )
+                dl_url = browser.find_element_by_xpath(
+                    downloadbutton_xpath
+                ).get_attribute("href")
                 self.write_to_file(dl_url, i, output_filename)
                 browser.quit()
             except TimeoutError:
                 print("Wait period lapsed")
                 browser.quit()
-             
+
     def get_driver(self):
 
         options = webdriver.ChromeOptions()
         options.add_argument("--ignore-certificate-error")
         options.add_argument("--ignore-ssl-errors")
-        
+
         if self.headless:
             options.add_argument("--headless")
-        
+
         return webdriver.Chrome(options=options)
 
     def scrape_em(self):
-    
-        if self.end != 'all':
-            self.song_urls_df = self.song_urls_df.iloc[self.start:self.end,]
+
+        if self.end != "all":
+            self.song_urls_df = self.song_urls_df.iloc[
+                self.start : self.end,
+            ]
 
         tic = time()
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -119,17 +139,15 @@ class FutureScraper():
 
         with ThreadPoolExecutor(max_workers=self.threads) as executor:
             for i in range(len(self.song_urls_df)):
-                futures.append(
-                    executor.submit(self.run_process, i, output_filename)
-                )
+                futures.append(executor.submit(self.run_process, i, output_filename))
         wait(futures)
         toc = time()
         tictoc = toc - tic
         print(f"Elapsed run time: {tictoc} seconds")
-        
 
 
-if __name__=="__main__":
-    scraper = FutureScraper(csv_name="missing_tracks.csv", headless=True, threads=1)
+if __name__ == "__main__":
+    scraper = FutureScraper(
+        csv_name="only_tracks.csv", end=10, headless=True, threads=1
+    )
     scraper.scrape_em()
-        
